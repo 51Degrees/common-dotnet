73205d8d141ae8db61483b20f1cf381408ba2d5b
James Rogers <JamesR@51degrees.com>
2025-09-23 13:35:13 +0100

    FEAT: Add TestDateTime so it can be used in more than just Pearl

---
+++ FiftyOne.Common.TestHelpers/TestDateTime.cs
@@ -0,0 +1,49 @@
+﻿using FiftyOne.Common.Wrappers;
+using System;
+
+namespace FiftyOne.Common.TestHelpers;
+
+/// <summary>
+/// Test implementation of <see cref="IDateTimeWrapper"/> used to artificially 
+/// control the time returned to workers and builders for testing.
+/// </summary>
+public class TestDateTime : IDateTimeWrapper
+{
+    public DateTime Now => Current.ToLocalTime();
+
+    public DateTime UtcNow => Current;
+
+    public DateTime Today => Current.Date;
+
+    /// <summary>
+    /// The current date time that the test service will return.
+    /// </summary>
+    public DateTime Current { get; private set; }
+
+    /// <summary>
+    /// Constructs a new instance of <see cref="TestDateTime"/>.
+    /// </summary>
+    /// <param name="dateTime"></param>
+    public TestDateTime(DateTime dateTime)
+    {
+        Current = dateTime;
+    }
+
+    /// <summary>
+    /// Increment the date time returned.
+    /// </summary>
+    /// <param name="increment"></param>
+    public void Increment(TimeSpan increment)
+    {
+        Current = Current.Add(increment);
+    }
+
+    /// <summary>
+    /// Explicitly sets the current datetime.
+    /// </summary>
+    /// <param name="value"></param>
+    public void Set(DateTime value)
+    {
+        Current = value;
+    }
+}
