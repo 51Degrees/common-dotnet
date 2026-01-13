18251d87370adb0d920d06662cee20509f231f7f
YaroslavVlasenko <yaroslavvlasenko@gmail.com>
2026-01-13 19:03:46 +0200

    BUG: Created FiftyOne.Common.FileUtils class with FindFile method. Created FiftyOne.Common.KeyUtils class with IsInvalidKey method. Created FiftyOne.Common.TestHelpers.TestUtils class with GetFilePath method, which contains test-specific file existence checking logic (using Assert.Inconclusive).

---
+++ FiftyOne.Common/KeyUtils.cs
@@ -0,0 +1,61 @@
+/* *********************************************************************
+ * This Original Work is copyright of 51 Degrees Mobile Experts Limited.
+ * Copyright 2026 51 Degrees Mobile Experts Limited, Davidson House,
+ * Forbury Square, Reading, Berkshire, United Kingdom RG1 3EU.
+ *
+ * This Original Work is licensed under the European Union Public Licence
+ * (EUPL) v.1.2 and is subject to its terms as set out below.
+ *
+ * If a copy of the EUPL was not distributed with this file, You can obtain
+ * one at https://opensource.org/licenses/EUPL-1.2.
+ *
+ * The 'Compatible Licences' set out in the Appendix to the EUPL (as may be
+ * amended by the European Commission) shall be deemed incompatible for
+ * the purposes of the Work and the provisions of the compatibility
+ * clause in Article 5 of the EUPL shall not apply.
+ *
+ * If using the Work as, or as part of, a network application, by
+ * including the attribution notice(s) required under Article 5 of the EUPL
+ * in the end user terms of the application under an appropriate heading,
+ * such notice(s) shall fulfill the requirements of that article.
+ * ********************************************************************* */
+
+using System;
+using System.Text;
+
+namespace FiftyOne.Common
+{
+    public static class KeyUtils
+    {
+        /// <summary>
+        /// Checks if the supplied 51Degrees resource key / license key is invalid.
+        /// Note that this cannot determine if the key is definitely valid, just whether it is
+        /// definitely invalid.
+        /// </summary>
+        /// <param name="key">
+        /// The key to check.
+        /// </param>
+        /// <returns></returns>
+        public static bool IsInvalidKey(string key)
+        {
+            try
+            {
+                if (key == null)
+                {
+                    return true;
+                }
+
+                byte[] data = Convert.FromBase64String(key);
+                string decodedString = Encoding.UTF8.GetString(data);
+
+                return key.Trim().Length < 19 ||
+                    decodedString.Length < 14;
+            }
+            catch (Exception)
+            {
+                return true;
+            }
+        }
+    }
+}
+
