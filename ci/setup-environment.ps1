4d51c7c319bffa6de06870cc4e7fd78448635e0e
Alexander Popel <74662418+pi-alexander-popel@users.noreply.github.com>
2026-05-29 12:55:31 +0300

    Merged Pull Request '#34 fix-publish-snk->main: ci: fix strong name key installation during publish'

--- ci/setup-environment.ps1
+++ ci/setup-environment.ps1
@@ -1,6 +1,1 @@
-param(
-    [Parameter(Mandatory)][string]$StrongNameKeyBase64
-)
-$ErrorActionPreference = "Stop"
-
-[IO.File]::WriteAllBytes("$PSScriptRoot/../51Degrees.snk", [Convert]::FromBase64String($StrongNameKeyBase64))
+Write-Host "No special environment setup is needed"
